da64bdf216f80f9e13a6d7445cadc53e2fa4318a
duxing <515235416@qq.com>
2018-10-29 18:03:53 +0800

    新页面

---
+++ pages/seek/index.js
@@ -0,0 +1,93 @@
+// pages/seek/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sd_crf:[
+      {
+        cls:"act"
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }, {
+        cls: ""
+      }
+    ]
+  },
+  dsd_df(e){
+    let idx=e.currentTarget.dataset.idx,
+      sd_crf = this.data.sd_crf
+    sd_crf.map(a=>{
+      a.cls=""
+    })
+    sd_crf[idx].cls="act"
+    this.setData({ sd_crf: sd_crf})
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
